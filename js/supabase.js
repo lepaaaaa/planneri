a5ade103521cd92b5754faa051af2153ef695ec8
lepaaaaa <leevi.mutru1@gmail.com>
2026-08-20 10:09:57 +0300

    Update supabase.js
    vakiomuuttujat supabase -osaan

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,9 @@
 const SUPABASE_URL = "https://zppzqlcenivwfxbmljdy.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0Q5_voRANWCpoOnD5hNL-Q_Afbho047";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_0Q5_voRANWCpoOnD5hNL-Q_Afbho047";
+
+export const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
